e987b79824af7f1ce8fc4a6b90f9e702ac312b23
Matteo Agosti <matteo.agosti@gmail.com>
2013-03-26 16:05:22 +0100

    Generating draft docs

---
+++ docs/api.js
@@ -0,0 +1,31 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "DOM",
+        "Editable",
+        "Selection"
+    ],
+    "modules": [
+        "core",
+        "dom",
+        "selection"
+    ],
+    "allModules": [
+        {
+            "displayName": "core",
+            "name": "core",
+            "description": "The Core module provides main functionalities of Editable.JS API."
+        },
+        {
+            "displayName": "dom",
+            "name": "dom",
+            "description": "The DOM module provides a cross-browser abstraction layer for DOM\nmanipulations and helpers for common tasks."
+        },
+        {
+            "displayName": "selection",
+            "name": "selection",
+            "description": "The Selection module provides a cross-browser abstraction layer for range\nand selection."
+        }
+    ]
+} };
+});
