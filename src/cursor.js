4ec47715af8dd2de553e87b6c58b1ec3f60d54aa
Matteo Agosti <matteo.agosti@gmail.com>
2013-03-26 17:54:49 +0100

    Cursor submodule stub

---
+++ src/cursor.js
@@ -0,0 +1,19 @@
+/**
+ * The Cursor module provides a cross-browser abstraction layer for cursor.
+ *
+ * @module core
+ * @submodule cursor
+ */
+
+
+Editable.cursor = (function() {
+  'use strict';
+
+  /**
+   * @class Cursor
+   * @static
+   */
+  return {
+
+  };
+})();
