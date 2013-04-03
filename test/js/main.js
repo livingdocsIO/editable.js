c336fdf01d377913f3d70c85cee3f8eaf7571699
Matteo Agosti <matteo.agosti@gmail.com>
2013-04-03 18:08:46 +0200

    Listening to event sample code

--- test/js/main.js
+++ test/js/main.js
@@ -3,6 +3,10 @@
 
     Editable.init();
     $("article>div>p").editable();
-
+    Editable.on('focus', function(par1, par2,par3) {
+      console.log('Focus event handler was triggered', par1, par2, par3);
+    }).on('blur', function() {
+      console.log('Blur event handler was triggered');
+    });
   });
 })(jQuery);
