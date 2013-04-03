6642d418b5c6372d28176d8fd33ae3cb4beee230
Matteo Agosti <matteo.agosti@gmail.com>
2013-04-03 18:09:17 +0200

    Leftovers

--- test/js/main.js
+++ test/js/main.js
@@ -3,8 +3,8 @@
 
     Editable.init();
     $("article>div>p").editable();
-    Editable.on('focus', function(par1, par2,par3) {
-      console.log('Focus event handler was triggered', par1, par2, par3);
+    Editable.on('focus', function() {
+      console.log('Focus event handler was triggered');
     }).on('blur', function() {
       console.log('Blur event handler was triggered');
     });
