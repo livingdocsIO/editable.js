9ea50e40b2064510c558e73356ccb6e56d104d57
Matteo Agosti <matteo.agosti@gmail.com>
2013-04-08 15:29:26 +0200

    New global 'behavior' where default events behavior is defined

---
+++ src/behavior.js
@@ -0,0 +1,51 @@
+var behavior = (function() {
+  return {
+    focus: function(element) {
+      console.log('Default focus behavior');
+    },
+
+    blur: function(element) {
+      console.log('Default blur behavior');
+    },
+
+    flow: function(element, action) {
+      console.log('Default flow behavior');
+    },
+
+    selection: function(element, selection) {
+      console.log('Default selection behavior');
+    },
+
+    cursor: function(element, cursor) {
+      console.log('Default cursor behavior');
+    },
+
+    insert: function(element, direction) {
+      console.log('Default insert behavior');
+    },
+
+    split: function(element, before, after) {
+      console.log('Default split behavior');
+    },
+
+    merge: function(element, direction) {
+      console.log('Default merge behavior');
+    },
+
+    empty: function(element) {
+      console.log('Default empty behavior');
+    },
+
+    'switch': function(element, direction) {
+      console.log('Default switch behavior');
+    },
+
+    move: function(element, selection, direction) {
+      console.log('Default move behavior');
+    },
+
+    clipboard: function(element, selection, action) {
+      console.log('Default clipboard behavior');
+    }
+  };
+})();
