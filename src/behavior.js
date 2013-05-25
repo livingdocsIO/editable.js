be4627e246017dc5c6f431fa3ec34b04aa53be19
Matteo Agosti <matteo.agosti@gmail.com>
2013-05-25 19:03:23 +0200

    After splitting a block moves the cursor to the right position

--- src/behavior.js
+++ src/behavior.js
@@ -77,9 +77,11 @@ var behavior = (function() {
 
     split: function(element, cursor, before, after) {
       var parent = element.parentNode;
+      var newStart = after.firstChild.firstChild;
       parent.insertBefore(before, element);
-      parent.insertBefore(after, element);
-      parent.removeChild(element);
+      parent.replaceChild(after, element);
+      cursor.moveBefore(newStart);
+      cursor.update();
     },
 
     merge: function(element, direction) {
