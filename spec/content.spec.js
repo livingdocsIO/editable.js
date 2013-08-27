ae4dafcdbe2b1f00530fdd9b145f1c380a0c865d
Matteo Agosti <matteo.agosti@gmail.com>
2013-08-27 10:20:49 +0200

    Test case to check for BR preservation when normalizing consecutive tags

--- spec/content.spec.js
+++ spec/content.spec.js
@@ -4,6 +4,7 @@ describe('Test content', function() {
     var plainWithSpace = $('<div>Plain <strong>text</strong> <strong>block</strong> example snippet</div>')[0];
     var nested = $('<div>Nested <strong><em>text</em></strong><strong><em>block</em></strong> example snippet</div>')[0];
     var nestedMixed = $('<div>Nested <strong>and mixed <em>text</em></strong><strong><em>block</em> <em>examples</em></strong> snippet</div>')[0];
+    var consecutiveNewLines = $('<div>Consecutive<br><br>new lines</div>')[0];
 
     it('should work with plain block', function() {
       var expected = $('<div>Plain <strong>textblock</strong> example snippet</div>')[0];
@@ -29,6 +30,13 @@ describe('Test content', function() {
     it('should work with nested blocks that mix other tags', function() {
       var expected = $('<div>Nested <strong>and mixed <em>textblock</em> <em>examples</em></strong> snippet</div>')[0];
       var actual = nestedMixed.cloneNode(true);
+      content.normalizeTags(actual);
+      expect(actual.innerHTML).toEqual(expected.innerHTML);
+    });
+
+    it('should not merge consecutive new lines', function() {
+      var expected = consecutiveNewLines.cloneNode(true);
+      var actual = consecutiveNewLines.cloneNode(true);
       content.normalizeTags(actual);
       expect(actual.innerHTML).toEqual(expected.innerHTML);
     });
