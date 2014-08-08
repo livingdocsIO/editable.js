899549ca26f7ea2514175cbbcef06c1ee10f1563
Lukas <lpeyer@gmail.com>
2014-08-08 16:20:52 +0200

    Add iterator

---
+++ spec/interator.spec.js
@@ -0,0 +1,91 @@
+describe('Iterator', function() {
+
+  // Helper methods
+  // --------------
+
+  // todo: remove
+  rangy.init()
+
+  var callnTimes = function(object, methodName, count) {
+    var returnValue;
+    while (count--) {
+      returnValue = object[methodName]()
+    }
+    return returnValue;
+  };
+
+
+  describe('constructor method', function() {
+
+    beforeEach(function() {
+      this.element = $('<div>a</div>')[0];
+      this.iterator = new Iterator(this.element);
+    });
+
+    it('sets its properties', function() {
+      expect(this.iterator.root).toEqual(this.element);
+      expect(this.iterator.current).toEqual(this.element);
+      expect(this.iterator.next).toEqual(this.element);
+    });
+  });
+
+
+  describe('getNext()', function() {
+
+    beforeEach(function() {
+      this.element = $('<div>a</div>')[0];
+      this.iterator = new Iterator(this.element);
+    });
+
+    it('returns the root on the first call', function() {
+      var current = this.iterator.getNext();
+      expect(current).toEqual(this.element);
+    });
+
+    it('returns the the first child on the second call', function() {
+      var current = callnTimes(this.iterator, 'getNext', 2);
+      expect(current).toEqual(this.element.firstChild);
+    });
+
+    it('returns undefined on the third call', function() {
+      var current = callnTimes(this.iterator, 'getNext', 3);
+      expect(current).toEqual(null);
+    });
+
+  });
+
+
+  describe('replaceCurrent() after using highlightText.wrapPortion()', function() {
+
+    it('replaces the text node', function() {
+      this.element = $('<div>a</div>')[0];
+      this.iterator = new Iterator(this.element);
+      var current = callnTimes(this.iterator, 'getNext', 2);
+      var replacement = highlightText.wrapPortion({
+        element: current,
+        offset: 0,
+        length: 1
+      });
+
+      this.iterator.replaceCurrent(replacement);
+      expect(this.iterator.current).toEqual(replacement);
+      expect(this.iterator.next).toEqual(null);
+    });
+
+    it('replaces the first character of longer a text node', function() {
+      this.element = $('<div>word</div>')[0];
+      this.iterator = new Iterator(this.element);
+      var current = callnTimes(this.iterator, 'getNext', 2);
+      var replacement = highlightText.wrapPortion({
+        element: current,
+        offset: 0,
+        length: 1
+      });
+
+      this.iterator.replaceCurrent(replacement);
+      current = this.iterator.getNext();
+      expect(current.data).toEqual('ord');
+    });
+
+  });
+});
---
+++ src/iterator.js
@@ -0,0 +1,34 @@
+var Iterator = (function() {
+
+  var Iterator = function(root) {
+    this.root = root;
+    this.current = this.next = this.root;
+  };
+
+  Iterator.prototype.getNext = function() {
+    var child, n;
+    n = this.current = this.next;
+    child = this.next = undefined;
+    if (this.current) {
+      if (child = n.firstChild) {
+        this.next = child;
+      } else {
+        while ((n !== this.root) && !(this.next = n.nextSibling)) {
+          n = n.parentNode;
+        }
+      }
+    }
+    return this.current;
+  };
+
+  Iterator.prototype.replaceCurrent = function(replacement) {
+    this.current = replacement;
+    this.next = undefined;
+    var n = this.current;
+    while ((n !== this.root) && !(this.next = n.nextSibling)) {
+      n = n.parentNode;
+    }
+  };
+
+  return Iterator;
+})();
