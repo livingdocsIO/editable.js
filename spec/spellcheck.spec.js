69411a0429b17da7b6d269589a9f317540300751
Lukas <lpeyer@gmail.com>
2014-08-13 17:57:19 +0200

    Add spellcheck spec

---
+++ spec/spellcheck.spec.js
@@ -0,0 +1,37 @@
+describe('Spellcheck', function() {
+
+  beforeEach(function() {
+    this.editable = new Editable();
+  });
+
+  describe('new instance', function() {
+
+    it('is created and has a reference to editable', function() {
+      var spellcheck = new Spellcheck(this.editable);
+      expect(spellcheck.editable).toEqual(this.editable);
+    });
+  });
+
+  describe('checkSpelling', function() {
+    beforeEach(function() {
+      this.div = $('<p>A simple sentence.</p>')[0];
+      this.spellcheck = new Spellcheck(this.editable, {
+        spellcheckService: function(text, callback) {
+          callback(['simple']);
+        }
+      });
+    });
+
+    it('highlights a match', function() {
+      this.spellcheck.checkSpelling(this.div);
+      expect( $(this.div).find('.spellcheck').length ).toEqual(1);
+    });
+
+    it('match highlights are marked with "remove"', function() {
+      this.spellcheck.checkSpelling(this.div);
+      var $spellcheck = $(this.div).find('.spellcheck').first();
+      var dataEditable = $spellcheck.attr('data-editable');
+      expect(dataEditable).toEqual('remove');
+    });
+  });
+});
