31efa760de530d9df0a1df05782fb9e6b80944d9
Lukas <lpeyer@gmail.com>
2015-02-11 17:51:26 +0100

    Use hostname '*' in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,8 +29,8 @@ module.exports = function(grunt) {
     connect: {
       options: {
         port: 9050,
-        // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
+        // Change this to '*' to access the server from outside.
+        hostname: '*',
         livereload: 35759 // Default livereload listening port: 35729
       },
       livereload: {
