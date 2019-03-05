26ec89585ea6db39cb7962b017b3597020c2316c
Lukas Buenger <lukasbuenger@gmail.com>
2019-03-05 10:44:32 +0100

    test: highlight position extraction

--- spec/highlighting.spec.js
+++ spec/highlighting.spec.js
@@ -1,12 +1,13 @@
 import $ from 'jquery'
 
+import Selection from '../src/selection'
+import rangy from 'rangy'
+import highlightSupport from '../src/highlight-support'
 import Editable from '../src/core'
 import Highlighting from '../src/highlighting'
 import WordHighlighter from '../src/plugins/highlighting/text-highlighting'
 
-
 describe('Highlighting', function () {
-
   // Specs
 
   beforeEach(() => {
@@ -14,16 +15,13 @@ describe('Highlighting', function () {
   })
 
   describe('new Highlighting()', () => {
-
     it('creates an instance with a reference to editable', () => {
       const highlighting = new Highlighting(this.editable, {})
       expect(highlighting.editable).toEqual(this.editable)
     })
-
   })
 
   describe('WordHighlighter', () => {
-
     beforeEach(() => {
       const markerNode = $('<span class="highlight"></span>')[0]
       this.highlighter = new WordHighlighter(markerNode)
@@ -56,11 +54,13 @@ describe('Highlighting', function () {
   })
 
   describe('highlightSupport', () => {
-
     beforeEach(() => {
       this.$div = $('<div>Foobarista</div>').appendTo(document.body)
       this.editable = new Editable()
       this.editable.add(this.$div)
+      const range = rangy.createRange()
+      range.selectNodeContents(this.$div[0])
+      this.selection = new Selection(this.$div[0], range)
     })
 
     afterEach(() => {
@@ -74,7 +74,7 @@ describe('Highlighting', function () {
         editableHost: this.$div[0],
         text: 'bari',
         highlightId: 'myId',
-        textRange: {start: 3, end: 7}
+        textRange: { start: 3, end: 7 }
       })
       const highlightSpan = this.$div.find('[data-word-id="myId"]')
       expect(highlightSpan.length).toEqual(1)
@@ -82,18 +82,42 @@ describe('Highlighting', function () {
       expect(startIndex).toEqual(3)
     })
 
+    it('can extract a text-based range object', () => {
+      const editableHost = this.$div[0]
+      this.editable.highlight({
+        editableHost,
+        text: 'bari',
+        highlightId: 'myId',
+        textRange: { start: 3, end: 7 }
+      })
+
+      this.editable.highlight({
+        editableHost,
+        text: 'Fo',
+        highlightId: 'mySecondId',
+        textRange: { start: 0, end: 2 }
+      })
+      const extractedHighlightPositions = highlightSupport.extractHighlightedRanges(editableHost, this.selection)
+      expect(extractedHighlightPositions.myId).toBeDefined()
+      expect(extractedHighlightPositions.myId.start).toEqual(3)
+      expect(extractedHighlightPositions.myId.end).toEqual(7)
+      expect(extractedHighlightPositions.mySecondId).toBeDefined()
+      expect(extractedHighlightPositions.mySecondId.start).toEqual(0)
+      expect(extractedHighlightPositions.mySecondId.end).toEqual(2)
+    })
+
     it('skips if a highlight with the given id is already present', () => {
       this.editable.highlight({
         editableHost: this.$div[0],
         text: 'bari',
         highlightId: 'myId',
-        textRange: {start: 3, end: 7}
+        textRange: { start: 3, end: 7 }
       })
       this.editable.highlight({
         editableHost: this.$div[0],
         text: 'Foobari',
         highlightId: 'myId',
-        textRange: {start: 0, end: 7}
+        textRange: { start: 0, end: 7 }
       })
       const highlightSpan = this.$div.find('[data-word-id="myId"]')
       expect(highlightSpan.length).toEqual(1)
@@ -105,11 +129,10 @@ describe('Highlighting', function () {
         editableHost: this.$div[0],
         text: 'bari',
         highlightId: 'myId',
-        textRange: {foo: 3, bar: 7}
+        textRange: { foo: 3, bar: 7 }
       })
       const highlightSpan = this.$div.find('[data-word-id="myId"]')
       expect(highlightSpan.length).toEqual(0)
     })
   })
-
 })
