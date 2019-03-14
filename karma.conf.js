f035cea2bc38ea54a2f1a80fd30a85fa877f3fc4
Marc Bachmann <marc.brookman@gmail.com>
2019-03-14 01:11:57 +0100

    chore: Use reporter that's better suited for CI

--- karma.conf.js
+++ karma.conf.js
@@ -34,7 +34,7 @@ module.exports = function (config) {
 
     webpackMiddleware: {noInfo: true},
 
-    reporters: ['progress', 'coverage'],
+    reporters: ['dots', 'coverage'],
 
     port: 9876,
 
