b60794f37c217b8b8c07673c072609a65f07b2b8
eileenoo <schlott.verena.eileen@gmail.com>
2025-02-19 09:04:10 +0100

    feat: add delay before smartQuotes are applied

--- src/smartQuotes.js
+++ src/smartQuotes.js
@@ -16,9 +16,19 @@ const shouldBeClosingQuote = (text, indexCharBefore) => text[indexCharBefore] &&
 
 const replaceQuote = (range, index, quoteType) => {
   const startContainer = range.startContainer
+  if (!startContainer.nodeValue) {
+    return
+  }
   const textNode = document.createTextNode(`${startContainer.nodeValue.substring(0, index)}${quoteType}${startContainer.nodeValue.substring(index + 1)}`)
-  range.startContainer.replaceWith(textNode)
+  startContainer.replaceWith(textNode)
   return textNode
+}
+
+// TODO: Fix ‹Didn’t› case -> only works with timeout
+const hasCharAfter = (textArr, offset) => {
+  console.log('textArr :>> ', textArr)
+  console.log('offset :>> ', offset)
+  return false
 }
 
 const hasSingleOpeningQuote = (textArr, offset, singleOpeningQuote) => {
@@ -28,12 +38,16 @@ const hasSingleOpeningQuote = (textArr, offset, singleOpeningQuote) => {
   for (let i = offset - 1; i >= 0; i--) {
     if (isSingleQuote(textArr[i]) && (!isApostrophe(singleOpeningQuote) && !isApostrophe(textArr[i]))) {
       return textArr[i] === singleOpeningQuote
+      // TODO: keep on looking for an unclosed single opening quote -> need to save single opening and closing quotes
+      // if (textArr[i] === singleOpeningQuote) {
+      //   return true
+      // }
     }
   }
   return false
 }
 
-export const applySmartQuotes = (range, config, char, target) => {
+export const applySmartQuotes = (range, config, char, target, carretOffset) => {
   const isCharSingleQuote = isSingleQuote(char)
   const isCharDoubleQuote = isDoubleQuote(char)
 
@@ -47,14 +61,16 @@ export const applySmartQuotes = (range, config, char, target) => {
   let newTextNode
 
   if (shouldBeClosingQuote(textArr, offset - 2)) {
-    // Don't transform single-quote if there is no respective single-opening-quote
-    if (isCharSingleQuote && !hasSingleOpeningQuote(textArr, offset, singleQuotes[0])) {
-      return
+    if (isCharSingleQuote) {
+      // Don't transform apostrophes
+      if (hasCharAfter(textArr, offset)) {
+        return
+      }
+      // Don't transform single-quote if there is no respective single-opening-quote
+      if (!hasSingleOpeningQuote(textArr, offset, singleQuotes[0])) {
+        return
+      }
     }
-    // TODO: Fix ‹Didn’t› case -> only works with timeout
-    // if (isCharSingleQuote && hasTextAfter(target, offset)) {
-    //   return
-    // }
     const closingQuote = isCharSingleQuote ? singleQuotes[1] : quotes[1]
     newTextNode = replaceQuote(range, offset - 1, closingQuote)
   } else if (shouldBeOpeningQuote(textArr, offset - 2)) {
@@ -69,5 +85,7 @@ export const applySmartQuotes = (range, config, char, target) => {
   // Resets the cursor
   const window = target.ownerDocument.defaultView
   const selection = window.getSelection()
-  selection.collapse(newTextNode, offset)
+  selection.collapse(newTextNode, carretOffset ?? offset)
 }
+
+// TODO: fix special case when quickly typing two quotes after each other
