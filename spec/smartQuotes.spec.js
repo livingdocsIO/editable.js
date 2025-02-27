e90ed16281b7787cdd08ed4e11d443f7cd47fc43
eileenoo <schlott.verena.eileen@gmail.com>
2025-02-27 16:07:55 +0100

    fix: added tests for replaceQuote and made its implementation more robust

--- spec/smartQuotes.spec.js
+++ spec/smartQuotes.spec.js
@@ -1,5 +1,5 @@
 import {expect} from 'chai'
-import {isDoubleQuote, isSingleQuote, isWhitespace, isSeparatorOrWhitespace, isApostrophe} from '../src/smartQuotes'
+import {isDoubleQuote, isSingleQuote, isWhitespace, isSeparatorOrWhitespace, isApostrophe, replaceQuote} from '../src/smartQuotes'
 
 const allSingleQuotes = ['‘', '’', '‹', '›', '‚', '‘', '›', '‹', `'`, `‘`]
 const allDoubleQuotes = ['«', '»', '»', '«', '"', '"', '“', '”', '”', '”', '“', '“', '„', '“']
@@ -8,7 +8,7 @@ const nonStringValues = [undefined, null, true, 123, NaN]
 const whitespaceChars = [' ', '\t', '\n', '\r', '\v', '\f']
 const separatorValues = ['>', '-', '–—']
 
-describe('Smart Quotes Helper Functions', () => {
+describe('Smart Quotes Helper Functions:', () => {
   describe('isDoubleQuote', () => {
     it('Should return false for non double quote values', () => {
       [...charValues, ...separatorValues, ...nonStringValues, ...allSingleQuotes].forEach(value => {
@@ -52,7 +52,7 @@ describe('Smart Quotes Helper Functions', () => {
   })
 
   describe('isSeparatorOrWhitespace', () => {
-    it('should return false for non whitespace / separator characters', () => {
+    it('should return false for non whitespace/ separator characters', () => {
       [...charValues, ...nonStringValues ].forEach(value => {
         expect(isSeparatorOrWhitespace(value)).to.equal(false, `Failed for: ${value}`)
       })
@@ -80,3 +80,37 @@ describe('Smart Quotes Helper Functions', () => {
   })
 })
 
+const createRangeWithText = (text) => {
+  const textNode = document.createTextNode(text)
+  const range = document.createRange()
+  range.selectNodeContents(textNode)
+  return range
+}
+
+describe('replaceQuote(): ', () => {
+  const testString = '123 "you'
+  const index = testString.indexOf('"')
+
+  it('should replace quote at given index', () => {
+    const range = createRangeWithText(testString)
+    const replacedTextNode = replaceQuote(range, index, '`')
+    expect(replacedTextNode.textContent).to.equal('123 `you')
+  })
+
+  it('should return null if range is invalid', () => {
+    const invalidNodeValue = replaceQuote(undefined, index, '`')
+    expect(invalidNodeValue).to.equal(null)
+  })
+
+  it('should return null if range is empty', () => {
+    const range = createRangeWithText('')
+    const replacedTextNode = replaceQuote(range, 0, '`')
+    expect(replacedTextNode).to.equal(null)
+  })
+
+  it('should insert quote at the end, if index is out of bounce', () => {
+    const range = createRangeWithText(testString)
+    const replacedTextNode = replaceQuote(range, 40, '`')
+    expect(replacedTextNode.textContent).to.equal(`${testString}${'`'}`)
+  })
+})
